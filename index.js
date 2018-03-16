1564a30b15330899ff21d09d6816b1eaab8a8f80
Aziz Kosber <a.kosber@gmail.com>
2018-03-16 19:14:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay (musicians, instruments) {
+  let finalArray = [];
+  for ( let i = 0; i < 4; i++ ){
+  finalArray.push(musicians[i] + " plays " + instruments[i]);
+}
+return finalArray
+}
+
+function johnLennonFacts(facts) {
+  while ([i < facts.length]) {
+    facts[i] = facts[i] + "!!!"
+  }
+  return facts
+}
