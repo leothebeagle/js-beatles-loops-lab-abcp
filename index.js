90a813668cfcc6fcfefae036461b087c74188210
Aziz Kosber <a.kosber@gmail.com>
2018-03-18 19:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   let talent = []
   for(let i = 0, x = musicians.length; i < x; i++) {
-    talent.push(`${musicians[i] plays ${instruments[i]}.}`);
+    talent.push(`${musicians[i]} plays ${instruments[i]}}`);
   }
   return talent
 }
